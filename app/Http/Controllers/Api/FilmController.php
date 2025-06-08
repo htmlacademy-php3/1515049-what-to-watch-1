b71c3a5b23c66faace5ffd7dc874fdf59bbf3840
Наталья Романова <Natalocka_ne@mail.ru>
2025-06-08 20:05:31 +0300

    module3-task1 completed

--- app/Http/Controllers/Api/FilmController.php
+++ app/Http/Controllers/Api/FilmController.php
@@ -12,4 +12,34 @@ class FilmController extends Controller
     {
         return response()->json(['message' => 'Список фильмов']);
     }
+
+    public function show(int $id) : JsonResponse
+    {
+        return response()->json(['message' => "инфо о фильме {$id}"]);
+    }
+
+    public function store(Request $request) : JsonResponse
+    {
+        return response()->json(['message' => 'Добавлен фильм']);
+    }
+
+    public function update(Request $request, int $id) : JsonResponse
+    {
+        return response()->json(['message' => 'Обновление инфо о фильме']);
+    }
+
+    public function similar(int $id) : JsonResponse
+    {
+        return response()->json(['message' => 'Список похожих фильмов']);
+    }
+
+    public function showPromo() : JsonResponse
+    {
+        return response()->json(['message' => 'просмотр промо']);
+    }
+
+    public function createPromo(Request $request, $film_id) : JsonResponse
+    {
+        return response()->json(['message' => "создание промо фильма {id}"]);
+    }
 }
