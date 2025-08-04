6ee388c26e30b1194c19d1222d260d5e7a09eaa0
Наталья Романова <Natalocka_ne@mail.ru>
2025-08-04 18:59:14 +0300

    Откат к прошлому рабочему коммиту

--- app/Exceptions/FilmsRepositoryException.php
+++ app/Exceptions/FilmsRepositoryException.php
@@ -4,8 +4,21 @@ namespace App\Exceptions;
 
 use Exception;
 
+/**
+ * Исключение, выбрасываемое при ошибках в репозитории фильмов.
+ *
+ * Может использоваться для обработки ошибок, связанных с получением,
+ * сохранением или обновлением данных фильмов в источнике данных.
+ *
+ * @package App\Exceptions
+ */
 final class FilmsRepositoryException extends Exception
 {
+    /**
+     * Возвращает HTTP-код ошибки по умолчанию.
+     *
+     * @return int HTTP-статус 500 (внутренняя ошибка сервера)
+     */
     public function getStatusCode(): int
     {
         return 500;
